c2a51e7f3d64a5c02946e4c5a7389aef587402fb
Jota <jota.smk@gmail.com>
2023-12-05 22:31:12 -0300

    add some DADA analysis

--- DIHE-clust.py
+++ DIHE-clust.py
@@ -45,7 +45,7 @@ def calculate_dihedral(trajectory, i, j, k, l):
         phi = np.arccos(np.clip(cos_phi, -1.0, 1.0)) # Clip for numerical stability
 
         phi_deg = np.degrees(phi)
-        dihedrals.append(phi_deg)
+        dihedrals.append(phi_deg+180.0)
 
     return np.array(dihedrals)
 
@@ -132,17 +132,52 @@ if ((file_format=='xyz') or (file_format=='netcdf')): #In this case a file with 
     for i in range(0,ndihe):
         print("--> Dihedral", i+1, "(out of ",ndihe, ")")
         dihetraj[:,i]=calculate_dihedral(coordinates,dihelist[i][0],dihelist[i][1],dihelist[i][2],dihelist[i][3])
-    print("\nThis results will be saved to 'dietraj.dat' file...\n")
+    print("\nThis results will be saved to 'dihetraj.dat' file...\n")
     fmt = ['%d'] + ['%.4f'] * ndihe
     indexes=np.arange(nsteps)
     np.savetxt('dihetraj.dat',np.column_stack((indexes,dihetraj)),fmt=fmt)
 else: # dihe case (a dihetraj file is provided
     # Open the file and read one line to determine the number of columns (dihe+1)
+    print("\nDihedrals time evolution will be read directly from input file\n")
+    print("\nReading file...\n")
     with open(input_name, 'r') as file:
       first_line = file.readline()
-      ndihe = len(first_line.split())-1
+    ndihe = len(first_line.split())-1
     # Load the data, skipping the first column
     dihetraj=np.loadtxt(input_name,usecols=range(1, ndihe+1))
     nsteps=len(dihetraj)
 
-#From now onwards, will be working with the dihetraj array (nsteps,ndihe)
+#From now onwards, we will be working with the dihetraj array (nsteps,ndihe)
+from dadapy import Data
+from dadapy import plot as pl
+import matplotlib.pyplot as plt
+
+dihetraj=dihetraj[::10]
+dihetraj = np.clip(dihetraj, 0.001, 359.999) # Clip for numerical stability
+dihetraj=dihetraj*np.pi/180.0 #Converts to radians
+
+print(np.max(dihetraj/(2*np.pi)))
+
+# initialise a Data object
+d_dihedrals = Data(dihetraj, verbose=False)
+# compute distances by setting the correct period
+d_dihedrals.compute_distances(maxk=dihetraj.shape[0]-1, period=2.*np.pi)
+# estimate the intrinsic dimension
+d_dihedrals.compute_id_2NN()
+# ID scaling analysig using two different methods
+ids_2nn, errs_2nn, scales_2nn = d_dihedrals.return_id_scaling_2NN()
+ids_gride, errs_gride, scales_gride = d_dihedrals.return_id_scaling_gride(range_max=1024)
+col = 'darkorange'
+plt.plot(scales_2nn, ids_2nn, alpha=0.85)
+plt.errorbar(scales_2nn, ids_2nn, errs_2nn, fmt='None')
+plt.scatter(scales_2nn, ids_2nn, edgecolors='k',s=50,label='2nn decimation')
+plt.plot(scales_gride, ids_gride, alpha=0.85, color=col)
+plt.errorbar(scales_gride, ids_gride, errs_gride, fmt='None',color=col)
+plt.scatter(scales_gride, ids_gride, edgecolors='k',color=col,s=50,label='2nn gride')
+plt.xlabel(r'Scale',size=15)
+plt.ylabel('Estimated ID',size=15)
+plt.xticks(size=15)
+plt.yticks(size=15)
+plt.legend(frameon=False,fontsize=14)
+plt.tight_layout()
+plt.show()
